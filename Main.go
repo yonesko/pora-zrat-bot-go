02c9054ddd45220c1851a2015a227a4d91c42ade
Gleb Danichev <glebio@yandex-team.ru>
2019-09-02 15:02:32 +0300

    UserName

--- Main.go
+++ Main.go
@@ -18,7 +18,7 @@ func main() {
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		log.Printf("UserName [%s] Text %s", update.Message.From.UserName, update.Message.Text)
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		msg.ReplyToMessageID = update.Message.MessageID
